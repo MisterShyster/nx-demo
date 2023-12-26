983d622462c115d5c2b1d852bfa3ceff4c21aa2a
Vadim Ermakov <vadim.ermakov@altenar.com>
2023-12-27 00:28:14 +0400

    added new lib1

--- apps/app2/src/app/app.tsx
+++ apps/app2/src/app/app.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
 import NxWelcome from './nx-welcome';
+import { lib1Function } from '@nx-demo/lib1';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const libContent = lib1Function();
   return (
     <StyledApp>
+      <h1>{libContent}</h1>
       <NxWelcome title="app2" />
     </StyledApp>
   );
---
+++ libs/lib1/src/lib/lib1.ts
@@ -0,0 +1,3 @@
+export const lib1Function = () => {
+  return "I am a content from lib1"
+}
